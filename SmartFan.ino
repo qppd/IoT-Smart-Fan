bc3eda8d76f1e8ee02ba2d2b462c1f58daa39a3c
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-09-02 21:09:35 +0800

    Added current sensor

--- SmartFan.ino
+++ SmartFan.ino
@@ -1,16 +1,24 @@
 #include <Arduino.h>
 #include "PinConfig.h"
 #include "DHTSensor.h"
+
 #include "FirebaseConfig.h"
+#include "CURRENTSensor.h"
+
 
 // Create DHT sensor object
 DHTSensor dhtSensor(DHT_PIN);
+
+// Create Current sensor object (ACS712 5A on pin 34 by default)
+#define CURRENT_SENSOR_PIN 34
+CURRENTSensor currentSensor(CURRENT_SENSOR_PIN);
 
 void setup()
 {
     Serial.begin(115200);
     dhtSensor.begin();
     initFirebase();
+    currentSensor.begin();
 }
 
 void loop()
@@ -18,12 +26,21 @@ void loop()
     float temperature = dhtSensor.readTemperature();
     float humidity = dhtSensor.readHumidity();
 
+    // Microtask: Read current
+    float current = currentSensor.readCurrent();
+
+
+    // Microtask: Print sensor values
     Serial.print("Temperature: ");
     Serial.print(temperature);
     Serial.print(" °C, Humidity: ");
     Serial.print(humidity);
-    Serial.println(" %");
+    Serial.print(" %, Current: ");
+    Serial.print(current, 3);
+    Serial.println(" A");
 
+
+    // Microtask: Send data to Firebase
     if (Firebase.RTDB.setFloat(&fbdo, "/temperature", temperature)) {
         Serial.println("Temperature sent to Firebase.");
     } else {
@@ -38,5 +55,12 @@ void loop()
         Serial.println(fbdo.errorReason());
     }
 
-    delay(2000); // Read every
+    if (Firebase.RTDB.setFloat(&fbdo, "/current", current)) {
+        Serial.println("Current sent to Firebase.");
+    } else {
+        Serial.print("Failed to send current: ");
+        Serial.println(fbdo.errorReason());
+    }
+
+    delay(2000); // Read every 2 seconds
 }
